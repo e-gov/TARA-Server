27f21815ee87774ee2356d0bee7e6d5a40eda01a
Janar Rahumeel <janar.rahumeel@cgi.com>
2018-01-09 15:34:50 +0200

    TARA-86

--- src/test/java/ee/ria/sso/config/TestTaraConfiguration.java
+++ src/test/java/ee/ria/sso/config/TestTaraConfiguration.java
@@ -3,10 +3,13 @@ package ee.ria.sso.config;
 import javax.annotation.PostConstruct;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.services.ServicesManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import ee.ria.sso.test.TestServicesManager;
 
 /**
  * @author Janar Rahumeel (CGI Estonia)
@@ -22,6 +25,11 @@ public class TestTaraConfiguration {
     @Bean
     public CasConfigurationProperties casConfigurationProperties() {
         return new CasConfigurationProperties();
+    }
+
+    @Bean
+    public ServicesManager testServicesManager() {
+        return new TestServicesManager();
     }
 
     @PostConstruct
