3369b0e694f1593159a31340d19549ad39ef8a8e
Risto Seene <39149669+rsarendus@users.noreply.github.com>
2018-06-27 13:18:13 +0300

    Fix TARAEI-148 disappearance during pull requests

--- src/main/java/ee/ria/sso/service/impl/ManagerServiceImpl.java
+++ src/main/java/ee/ria/sso/service/impl/ManagerServiceImpl.java
@@ -30,8 +30,13 @@ public class ManagerServiceImpl extends AbstractService implements ManagerServic
     @Override
     public Optional<OidcRegisteredService> getServiceByID(String serviceID) {
         this.log.debug("Searching OIDC service by <{}>", serviceID);
-        Optional<OidcRegisteredService> service = Optional.ofNullable(this.servicesManager.findServiceBy(serviceID,
-            OidcRegisteredService.class));
+        Optional<OidcRegisteredService> service;
+        try {
+            service = Optional.ofNullable(this.servicesManager.findServiceBy(serviceID, OidcRegisteredService.class));
+        } catch (RuntimeException e) {
+            this.log.error("Internal CAS error", e);
+            service = Optional.empty();
+        }
         this.log.debug("Service has been found? <{}>", service.isPresent());
         return service;
     }
--- src/test/java/ee/ria/sso/config/TaraPropertiesTest.java
+++ src/test/java/ee/ria/sso/config/TaraPropertiesTest.java
@@ -1,9 +1,17 @@
 package ee.ria.sso.config;
 
 import ee.ria.sso.AbstractTest;
+import ee.ria.sso.service.ManagerService;
+import org.apereo.cas.services.OidcRegisteredService;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.webflow.core.collection.ParameterMap;
+import org.springframework.webflow.execution.RequestContext;
+import org.springframework.webflow.execution.RequestContextHolder;
+
+import java.util.Optional;
 
 /**
  * Created by Janar Rahumeel (CGI Estonia)
@@ -17,6 +25,38 @@ public class TaraPropertiesTest extends AbstractTest {
     @Test
     public void testApplicationVersion() {
         Assert.assertNotEquals("Is not different", "-", this.taraProperties.getApplicationVersion());
+    }
+
+    @Test
+    public void getHomeUrl_invalidServiceUrl_shouldReturnEmptyHomeUrl() {
+        setRequestContextWith("invalid\\service\\url");
+        Assert.assertEquals("#", this.taraProperties.getHomeUrl());
+    }
+
+    @Test
+    public void getHomeUrl_validServiceUrl_shouldReturnValidHomeUrl() {
+        OidcRegisteredService oidcRegisteredService = Mockito.mock(OidcRegisteredService.class);
+        Mockito.when(oidcRegisteredService.getInformationUrl()).thenReturn("https://client/url");
+
+        ManagerService managerService = Mockito.mock(ManagerService.class);
+        Mockito.when(managerService.getServiceByID("https://client/url"))
+                .thenReturn(Optional.of(oidcRegisteredService));
+
+        TaraProperties taraProperties = new TaraProperties(null, null, managerService);
+
+        setRequestContextWith("https://service/url?redirect_uri=https%3A%2F%2Fclient%2Furl");
+        Assert.assertEquals("https://client/url", taraProperties.getHomeUrl());
+    }
+
+    private static void setRequestContextWith(String serviceUrl) {
+        ParameterMap parameterMap = Mockito.mock(ParameterMap.class);
+        Mockito.when(parameterMap.getRequired("service")).thenReturn(serviceUrl);
+        Mockito.when(parameterMap.contains("service")).thenReturn(true);
+
+        RequestContext requestContext = Mockito.mock(RequestContext.class);
+        Mockito.when(requestContext.getRequestParameters()).thenReturn(parameterMap);
+
+        RequestContextHolder.setRequestContext(requestContext);
     }
 
 }
