e7a659df63c976fea91bd61a603662a2b4c0a5ca
Risto Seene <39149669+rsarendus@users.noreply.github.com>
2018-08-02 12:52:31 +0300

    AUT-2 - no ID-card error message (#59)
    * AUT-2 - no ID-card error message - initial
    
    * AUT-2 - no ID-card error message - improvements
    
    * AUT-2 - Russian translation

---
+++ src/main/java/ee/ria/sso/authentication/AuthenticationFailedException.java
@@ -0,0 +1,21 @@
+package ee.ria.sso.authentication;
+
+public class AuthenticationFailedException extends RuntimeException {
+
+    private final String errorMessageKey;
+
+    public AuthenticationFailedException(String errorMessageKey, String message) {
+        super(message);
+        this.errorMessageKey = errorMessageKey;
+    }
+
+    public AuthenticationFailedException(String errorMessageKey, String message, Throwable cause) {
+        super(message, cause);
+        this.errorMessageKey = errorMessageKey;
+    }
+
+    public String getErrorMessageKey() {
+        return errorMessageKey;
+    }
+
+}
