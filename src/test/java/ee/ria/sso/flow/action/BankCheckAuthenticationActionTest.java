dd8b038c6e4fab3e8bb7ecfd313a1a64986e6580
priit.reiser <priitr@gmail.com>
2018-09-10 11:48:29 +0300

    AUT-104 - check for Pac4jConstants.REQUESTED_URL (and that the session still exists) before the login flow is continued.

---
+++ src/test/java/ee/ria/sso/flow/action/BankCheckAuthenticationActionTest.java
@@ -0,0 +1,33 @@
+package ee.ria.sso.flow.action;
+
+import ee.ria.sso.service.banklink.BanklinkAuthenticationService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BankCheckAuthenticationActionTest extends AbstractAuthenticationActionTest {
+
+    @Mock
+    BanklinkAuthenticationService banklinkAuthenticationService;
+
+    @InjectMocks
+    BankCheckAuthenticationAction action;
+
+    @Test
+    public void success() throws Exception {
+        getAction().doExecute(requestContext);
+        verify(banklinkAuthenticationService).checkLoginForBankLink(eq(requestContext));
+    }
+
+    @Override
+    AbstractAuthenticationAction getAction() {
+        return action;
+    }
+}
