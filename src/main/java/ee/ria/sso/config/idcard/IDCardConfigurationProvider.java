b5e76abbfa8fefee6ba964523994f59e237ba939
Risto Seene <39149669+rsarendus@users.noreply.github.com>
2018-09-19 16:49:59 +0300

    AUT-109 - improvements & documentation

--- src/main/java/ee/ria/sso/config/idcard/IDCardConfigurationProvider.java
+++ src/main/java/ee/ria/sso/config/idcard/IDCardConfigurationProvider.java
@@ -2,12 +2,17 @@ package ee.ria.sso.config.idcard;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 import org.springframework.validation.annotation.Validated;
 
+import javax.annotation.PostConstruct;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,13 +25,25 @@ import java.util.List;
 @Setter
 public class IDCardConfigurationProvider {
 
-    private boolean ocspEnabled;
+    private boolean ocspEnabled = true;
 
-    private String ocspUrl = "http://demo.sk.ee/ocsp";
+    private String ocspUrl;
     private String ocspCertificateLocation;
     private List<String> ocspCertificates = new ArrayList<>();
 
-    private long ocspAcceptedClockSkew = 2;
-    private long ocspResponseLifetime = 900;
+    @Min(0L)
+    private long ocspAcceptedClockSkew = 2L;
+
+    @Min(0L)
+    private long ocspResponseLifetime = 900L;
+
+    @PostConstruct
+    public void validateConfiguration() {
+        if (this.ocspEnabled) {
+            Assert.notNull(this.ocspUrl, "OCSP URL cannot be missing!");
+            Assert.notNull(this.ocspCertificateLocation, "OCSP certificates location cannot be missing!");
+            Assert.notEmpty(this.ocspCertificates, "List of OCSP certificates cannot be empty!");
+        }
+    }
 
 }
