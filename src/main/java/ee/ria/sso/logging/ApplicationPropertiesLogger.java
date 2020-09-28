c67f047561653438c6b09866f67f8f967a85abf9
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2020-09-28 13:22:52 +0300

    AUT-77 - Log application properties on startup (#146)
    * AUT-77 - Log application properties on startup
    * AUT-77 - Hide pass and key entries

---
+++ src/main/java/ee/ria/sso/logging/ApplicationPropertiesLogger.java
@@ -0,0 +1,54 @@
+package ee.ria.sso.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.text.StringEscapeUtils;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.CompositePropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Slf4j
+@Component
+public class ApplicationPropertiesLogger {
+
+    @EventListener
+    public void listenContextRefreshedEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        logApplicationProperties((ConfigurableEnvironment) contextRefreshedEvent.getApplicationContext().getEnvironment());
+    }
+
+    private void logApplicationProperties(ConfigurableEnvironment environment) {
+
+        Collection<PropertySource<?>> propertySources = new ArrayList<>();
+
+        environment.getPropertySources().forEach(propertySource -> {
+            if (propertySource instanceof CompositePropertySource && propertySource.getName().contains("bootstrapProperties")) {
+                propertySources.addAll(((CompositePropertySource) propertySource).getPropertySources());
+            }
+        });
+
+        propertySources
+                .forEach(propertySource -> {
+                    try {
+                        (((CompositePropertySource) propertySource).getPropertySources())
+                                .forEach(propSource ->
+                                Arrays.stream(((EnumerablePropertySource) propSource).getPropertyNames())
+                                        .forEach(property -> {
+                                            if (property.contains("pass") || property.contains("key")) {
+                                                log.info("{}: {}", property, "***");
+                                            } else {
+                                                log.info("{}: {}", property, StringEscapeUtils.escapeJava(environment.getProperty(property)));
+                                            }
+                                        }));
+                    } catch (Exception e) {
+                        log.error("{}", e.getMessage());
+                    }
+                });
+    }
+}
